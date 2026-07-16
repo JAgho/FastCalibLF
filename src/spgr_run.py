7c8fe76378fec73280a9409b36c68d8ac8149d4f
AntoineTheb <toreil@me.com>
2026-07-16 10:39:26 +0100

    Add missing files

--- src/spgr_run.py
+++ src/spgr_run.py
@@ -1,10 +1,8 @@
 # %%
+import runpy
 import numpy as np
-#all other scanner imports
-
 import console
-from console.servixe.acquisition_manager import AcquisitionControlManager
-
+from console.service.acquisition_manager import AcquisitionControlManager
 from params import ScanParams
 from read_data import read_data
 from make_magnitude import make_magnitude
@@ -12,13 +10,36 @@ from make_phase_ramps import make_phase_ramps
 
 def main():
     # TBD
-    seq_params = {
-        "readout_length": 0,
-        "n_averages": 0,
-        "coords": 0,
-        "dummies": 0
-    }
+    params = {
+    "flip_angle": np.pi / 6,
+    "rf_duration": 400e-6,
+    "tr1": 20e-3,
+    "tr2": 100e-3,
+    "ro_bandwidth": 20e3,
+    "n_samples": 256,
+    "fov_projection": 250e-3,
+    "delay_rf_to_prephaser": 0.0,
+    "delay_prephaser_to_readout": 0.0,
+    "delay_readout_to_spoiler": 0.0,
+    "double_echo_gap": 0.0,
+    "spoiler_area_factor": 4.0,
+    "ramp_duration": 200e-6,
+    "gradient_correction": 0.0,
+    "dummies": 3,
+    "averages": 4,
+}
 
+    seq = afi_calibration.constructor(**params)
+
+    seq_base = seq
+
+
+    #nake f0 version that diesnt plot
+    runpy.run_path("/home/openimaging/Code/OpenImaging/utilities/calibration/f0_determination.py")
+
+    with AcquisitionControlManager() as mngr:
+        mngr.acquisition.set_sequence(sequence=seq, parameter=console.parameter)
+        acq_data = mngr.acquisition.run(store_unprocessed=False)
     calib_params = ScanParams(console)
 
     for i in range(3): 
